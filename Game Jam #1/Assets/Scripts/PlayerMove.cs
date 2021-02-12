b9cfb94026a09a7964b6426905d311b231ad3c36
Shadow7898 <45573393+Shadow7898@users.noreply.github.com>
2021-02-12 16:41:58 -0700

    New version

---
+++ Game Jam #1/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xforce = Input.GetAxis("Horizontal");
+        float zforce = Input.GetAxis("Vertical");
+
+        body.AddForce(new Vector3(xforce * speed, 0f, zforce * speed));
+    }
+}
